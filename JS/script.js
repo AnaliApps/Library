4f5c8db801e9268d7f48684f841b5c358f664370
AnaliApps <ronoanali5@gmail.com>
2024-04-09 22:22:02 +0300

    Fully refactored code to use class to create Book
    The project now utilizes class and a module pattern for the rest of the function. I used class to create Book object and also added one static method to retrieve unique id everytime a book is added into the library. The project now works as expected.

--- JS/script.js
+++ JS/script.js
@@ -6,123 +6,102 @@ class Book{
         this.id = id;
         this.status = status;
     }
-
-    getTitle(){
-        return this.title;
-    }
-    getAuthor(){
-        return this.author;
-    }
-    getPages(){
-        return this.pages;
-    }
-    getId(){
+    static getId(){
         this.id = Math.floor((Math.random() * 1000000) + 1);
         return this.id;
     }
-    getStatus(){
-        return this.status
-    }
 }
 
-let b = new Book("Lost ring","anali",100,12312312,"Yes")
-console.log(b.getTitle())
-console.log(b.getAuthor())
-console.log(b.getPages())
-console.log(b.getId());
-console.log(b.getStatus())
 
-// let myLibrary = [];
-// let bookSubmit = document.getElementById("bookinfo");
-// let card = document.querySelector(".card-container");
-// let formCont = document.querySelector(".form-container");
-// let btnAdd = document.querySelector("#add");
-// function Book(title,author,pages,id,status){
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.id = id;
-//     this.status = status;
-// }
-// function bookRead(){
-//     let ele = document.getElementsByName("read");
-//     let res = '';
-//     ele.forEach((item)=>{
-//         if(item.checked){
-//             res += item.value;
-//         }
-//     })
-//     return res
-// }
-// function refreshArr(Lib){
-//     return Lib.forEach((item)=>{
-//         let postCard = document.createElement("div");
-//         postCard.classList.add("innerCard");
-//         postCard.innerHTML = `
-//         <p class="id">${item.id}</p>
-//         <p>Title : ${item.title}</p>
-//         <p>Author: ${item.author}</p>
-//         <p>Pages : ${item.pages}</p>
-//         <p class="read">Read  : ${item.status}</p>
-//         `
-//         let btnDel = document.createElement("button")
-//         btnDel.textContent = "Delete";
-//         btnDel.classList.add = "btnDel";
-//         let statusBtn = document.createElement("button")
-//         statusBtn.textContent = "Read";
-//         statusBtn.classList.add = "statusBtn";
-//         postCard.appendChild(btnDel)
-//         postCard.appendChild(statusBtn);
-//         card.appendChild(postCard)
-//     })
-// }
-// function addBookToLibrary(){
-//     let titleOfBook = document.getElementById("title").value;
-//     let bookAuthor = document.getElementById("author").value;
-//     let bookPages = document.getElementById("pages").value;
-//     let id = Math.floor((Math.random() * 1000000) + 1);
-//     let status = bookRead();
-//     let book = new Book(titleOfBook,bookAuthor,bookPages,id,status);
-//     myLibrary = [...myLibrary,book]
-//     console.log(myLibrary)
-//     refreshArr(myLibrary)
-// }
+(function(){
+    let myLibrary = [];
+    let bookSubmit = document.getElementById("bookinfo");
+    let card = document.querySelector(".card-container");
+    let formCont = document.querySelector(".form-container");
+    let btnAdd = document.querySelector("#add");
+    let search = document.querySelector("#search").value;
 
-// btnAdd.addEventListener("click",()=>{
-//     formCont.style.left = "0%";
-//     formCont.style.display = "block";
-// });
-// bookSubmit.addEventListener("submit",(e)=>{
-//     card = document.querySelector(".card-container");
-//     e.preventDefault();
-//     card.textContent = ''
-//     addBookToLibrary();
-//     formCont.style.display = "none";
-//     card.style.width = "100vw";
-// })
-// function toggle(choice){
-//     let res = ''
-//     if(choice === "No"){
-//         res = "Yes"
-//     }else if(choice === "Yes"){
-//         res = "No"
-//     }
-//     return res;
-// }
-// function deleteOrToggleBtn(){
-//     card.addEventListener("click",(e)=>{
-//             myLibrary.forEach((item,index)=>{
-//                 if((item.id === parseInt(e.target.parentNode.children[0].textContent)&&(e.target.textContent === "Delete"))){
-//                     myLibrary.splice(index,1)
-//                     e.target.parentNode.remove()
-//                     console.log(myLibrary)
-//                 }else if(item.id === parseInt(e.target.parentNode.children[0].textContent)&&(e.target.textContent === "Read")){
-//                     toggle(item.status)
-//                     item.status = toggle(item.status)
-//                     e.target.parentNode.children[4].textContent = `Read : ${item.status}`;
-//                  }
-//             })
+function bookRead(){
+    let ele = document.getElementsByName("read");
+    let res = '';
+    ele.forEach((item)=>{
+        if(item.checked){
+            res += item.value;
+        }
+    })
+    return res
+}
 
-//     })
-// }
-// deleteOrToggleBtn()
+function addBookToLibrary(){
+    let titleOfBook = document.getElementById("title").value;
+    let bookAuthor = document.getElementById("author").value;
+    let bookPages = document.getElementById("pages").value;
+    let bookStatus = bookRead();
+    let book = new Book(titleOfBook,bookAuthor,bookPages,Book.getId(),bookStatus)
+    myLibrary = [...myLibrary,book]
+    displayUI(myLibrary);
+}
+
+function displayUI(Lib){
+    return Lib.forEach((item)=>{
+                let postCard = document.createElement("div");
+                postCard.classList.add("innerCard");
+                postCard.innerHTML = `
+                <p class="id">${item.id}</p>
+                <p>Title : ${item.title}</p>
+                <p>Author: ${item.author}</p>
+                <p>Pages : ${item.pages}</p>
+                <p class="read">Read  : ${item.status}</p>
+                `
+                let btnDel = document.createElement("button")
+                btnDel.textContent = "Delete";
+                btnDel.classList.add = "btnDel";
+                let statusBtn = document.createElement("button")
+                statusBtn.textContent = "Read";
+                statusBtn.classList.add = "statusBtn";
+                postCard.appendChild(btnDel)
+                postCard.appendChild(statusBtn);
+                card.appendChild(postCard)
+            })
+}
+
+function toggleStatus(choice){
+        let res = ''
+        if(choice === "No"){
+            res = "Yes"
+        }else if(choice === "Yes"){
+            res = "No"
+        }
+        return res;
+}
+
+function deleteOrToggleBtn(){
+    card.addEventListener("click",(e)=>{
+            myLibrary.forEach((item,index)=>{
+                if((item.id === parseInt(e.target.parentNode.children[0].textContent)&&(e.target.textContent === "Delete"))){
+                    myLibrary.splice(index,1)
+                    e.target.parentNode.remove()
+                }else if(item.id === parseInt(e.target.parentNode.children[0].textContent)&&(e.target.textContent === "Read")){
+                    item.status = toggleStatus(item.status)
+                    e.target.parentNode.children[4].textContent = `Read : ${item.status}`;
+                 }
+            })
+
+    })
+}
+
+bookSubmit.addEventListener("submit",e =>{
+    card = document.querySelector(".card-container");
+    e.preventDefault();
+    card.textContent = ''
+    addBookToLibrary()
+    formCont.style.display = "none";
+    card.style.width = "100vw";
+})
+
+btnAdd.addEventListener("click",()=>{
+    formCont.style.left = "0%";
+    formCont.style.display = "block";
+});
+deleteOrToggleBtn()
+})()
